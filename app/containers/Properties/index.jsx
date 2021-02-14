db8f4620f84feb441040b75e031ac14a875337a1
Ogadimma Osuji <orgmatter@gmail.com>
2021-02-14 16:11:04 +0000

    CORS fixed

--- app/containers/Properties/index.jsx
+++ app/containers/Properties/index.jsx
@@ -85,7 +85,7 @@ export class Properties extends React.PureComponent {
           </tr>
         </thead>
         <tbody>
-          {this.props.properties.properties.asset.map(property => (
+          {this.props.properties.asset.map(property => (
             <tr>
               <StyledTD style={{ width: '56px' }}>
                 <AssetLogo
